9ffd103e6fa05ef7a2335b3e1a9ba57c9844a6b5
Andrew Vos <andrew.vos@gmail.com>
2012-11-18 23:55:58 +0000

    pass arguments correctly

--- lib/metherd-missing.rb
+++ lib/metherd-missing.rb
@@ -3,11 +3,11 @@ require "metherd-missing/levenshtein_distance"
 require "metherd-missing/trie_node"
 
 module Kernel
-  def method_missing method
+  def method_missing(method, *args, &block)
     possible_method_names = self.methods.map(&:to_s)
     results = MetherdMissing::LevenshteinDistance.new(possible_method_names).search(method, 1)
     if results.any?
-      self.send(results.keys.first.to_s)
+      self.send(results.keys.first.to_s, *args, &block)
     else
       super
     end
